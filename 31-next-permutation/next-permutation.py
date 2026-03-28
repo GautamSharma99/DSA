cd1dadd66b61f824fef11251539decdc420de42e
GautamSharma99 <148205237+GautamSharma99@users.noreply.github.com>
2026-03-29 00:12:04 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (41.52%) - LeetSync

---
+++ 31-next-permutation/next-permutation.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def nextPermutation(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        pivot=-1
+        n=len(nums)
+        for i in range(n-2,-1,-1):
+            if nums[i]<nums[i+1]:
+                pivot=i
+                break
+
+        if pivot==-1:
+            nums.reverse()
+            return
+
+        for i in range(n-1,pivot,-1):
+            if nums[i]>nums[pivot]:
+                nums[i],nums[pivot]=nums[pivot],nums[i]
+                break
+
+        nums[pivot+1:]=reversed(nums[pivot+1:])
+
+
+            
+        
