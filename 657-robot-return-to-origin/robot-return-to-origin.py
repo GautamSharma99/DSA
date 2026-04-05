43b3b567fca73bec7eaa377b2c4d1ce6d5c81833
GautamSharma99 <148205237+GautamSharma99@users.noreply.github.com>
2026-04-05 15:19:32 +0530

    Time: 43 ms (17.83%) | Memory: 12.4 MB (99.71%) - LeetSync

---
+++ 657-robot-return-to-origin/robot-return-to-origin.py
@@ -0,0 +1,7 @@
+class Solution:
+    def judgeCircle(self, moves):
+        count = Counter(moves)
+        if (count['L'] == count['R']) and (count['U'] == count['D']):
+            return True
+        return False
+        
