3fed80bdcdfa400cd3ef99ae329d21ad02e5dc7d
Gautam Sharma <148205237+GautamSharma99@users.noreply.github.com>
2026-05-23 17:57:43 +0530

    Time: 0 ms (100.00%) | Memory: 44 MB (10.65%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -32,10 +32,7 @@ class Solution {
 
         while(start <= end) {
 
-            int mid = start + (end - start) / 2;
-
-            // Case 1
-            if(mid < end && nums[mid] > nums[mid + 1]) {
+            int mid = start + (end - start) / 2;if(mid < end && nums[mid] > nums[mid + 1]) {
                 return mid;
             }
 
